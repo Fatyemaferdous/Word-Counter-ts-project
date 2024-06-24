5efc5e097820858a78285c3a24a5e694c96b7bf4
Fatima Firdous <fmalaika64@gmail.com>
2024-06-24 21:14:27 +0500

    updated

---
+++ index.ts
@@ -0,0 +1,12 @@
+import inquirer from "inquirer"
+
+const ans : {Sentence : string} = await inquirer.prompt(
+    {
+name: "Sentence",
+type: "input",
+message:"Enter your sentence to count the  words"
+})
+
+const words = ans.Sentence.trim().split(" ")
+console.log(words)
+console.log(`your word count is ${words.length}`)
